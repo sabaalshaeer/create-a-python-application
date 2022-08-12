1026fe85272123f4ae2f55f3262f0b61735da87c
Saba Alshaeer <98250632+Sabaalshaeer@users.noreply.github.com>
2022-08-12 11:48:59 -0500

    Update wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -9,10 +9,7 @@ user_input = input("Please enter the path and name of the text file you want"
 # Sending user's input back to them,for now.
 print(user_input)
 
-common_word = input("would you like to strip common words from the results? (Y/N) ")
 
-print(common_word)
 
-user_output = input("\nWould you like to output these results to a file? (Y/N) ")
 
-print(user_output)
+
